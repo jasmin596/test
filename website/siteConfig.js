130ea28df7056eef1fb2eb97bc6588edf1bd970c
jasmin596 <skjasmin596@gmail.com>
2020-05-29 12:16:07 +0530

    commited

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'AI', // Title for your website.
   tagline: 'AI for u',
   url: 'https://test.github.io', // Your website URL
-  baseUrl: '/ai/', // Base URL for your project */
+  baseUrl: '/test/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'ai',
+  projectName: 'test',
   organizationName: 'jasmin596',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
